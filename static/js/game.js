a52a5cb209672677b8eaa345962a6cdac9a691ab
LilyG <ttiibe04@gmail.com>
2017-10-22 21:42:37 +0300

    Update game.js

--- static/js/game.js
+++ static/js/game.js
@@ -1,19 +1,16 @@
 (function() {
   var socket = io();
-
   var c = document.getElementById('container');
   var setHost = document.getElementById('set-host');
   var setPlayer = document.getElementById('set-player');
-
   setHost.addEventListener('click', setHostEventListener);
   setPlayer.addEventListener('click', setPlayerEventListener);
 
   function setHostEventListener(event) {
     event.stopPropagation();
-    c.innerHTML = '<canvas id="game"></canvas>';
+    c.innerHTML = '<canvas id="game"></canvas> ';
     setHost.removeEventListener('click', setHostEventListener);
     setPlayer.removeEventListener('click', setPlayerEventListener);
-    //fullScreen();
     setupHost();
   }
 
@@ -27,14 +24,9 @@
   }
 
   function setupHost() {
-    socket.emit('host game', { gameName: 'adam' });
-
-
-   //startScreen();
-
+    socket.emit('host game', { gameName: 'lily' });
     var players = [];
     playerPlacementOrder = ['left', 'right']; //, 'bottom', 'top'];
-
     var wallL=0,
       wallR=0,
       point1=0,
@@ -42,13 +34,10 @@
       over = true,
       startBtn = {},
       restartBtn={} ,
-      GameOver = false
-      ;
-
-
+      GameOver = false;
     socket.on('new player joined', function(data) {
-      console.log('new player joined', data);
-      players.push(setupPlayer({
+    console.log('new player joined', data);
+    players.push(setupPlayer({
         name: data.playerName,
         color: data.playerColor,
         placement: playerPlacementOrder[players.length],
@@ -65,20 +54,19 @@
     socket.on('orientation data', function(data) {
       var player = players.filter(function(p) { return p.name === data.playerName; })[0];
       if (!player) return console.log('Player not found', data.playerName);
-
       var tilt = data.beta;
       if (tilt < -45) tilt = -45;
       if (tilt > 45) tilt = 45;
       tilt += 45;
-
       player.pos = tilt / 90;
     });
 
-    // Add mousemove and mousedown events to the canvas
+    // Add mousedown events to the canvas
       document.addEventListener('click', btnClick, true);
-  //    canvas.addEventListener("click", btnClick, true);
 
-
+  // Initialise the collision sound
+  collision = document.getElementById("collide");
+    var img = document.getElementById("pic");
     var canvas = document.getElementById('game');
     var ctx = canvas.getContext('2d');
 
@@ -91,7 +79,7 @@
 
     function getGameBoardSize() {
       var vp = getViewPortSize();
-      return { x: 4, y: 4, w: vp.w - 8, h: vp.h - 8, border: 4, vp: vp };
+      return { x: 40, y: 4, w: vp.w - 80, h: vp.h - 8, border: 4, vp: vp };
     }
 
     function setViewPort() {
@@ -102,9 +90,7 @@
 
     setViewPort();
     window.onresize = setViewPort;
-
     var ball = {};
-
     function setupPlayer(options) {
       var player = {
         name: options.name,
@@ -124,9 +110,8 @@
         var pos;
         if (player.placement === 'top')    pos = { x: gs.x + gs.w * player.pos, y: -player.offset };
         if (player.placement === 'bottom') pos = { x: gs.x + gs.w * player.pos, y: gs.vp.h + player.offset };
-        if (player.placement === 'left')   pos = { x:0, y: gs.y + gs.h * player.pos };
-        if (player.placement === 'right')  pos = { x: gs.vp.w + 40, y: gs.y + gs.h * player.pos };
-
+        if (player.placement === 'left')   pos = { x:gs.x, y: gs.y + gs.h * player.pos };
+        if (player.placement === 'right')  pos = { x: gs.vp.w + gs.x, y: gs.y + gs.h * player.pos };
         if (pos.x + player.w > gs.x + gs.w) pos.x = gs.w - player.w + gs.x;
         if (pos.y + 100 > gs.y + gs.h) pos.y = gs.h -100 + gs.y;
         return pos;
@@ -148,39 +133,25 @@
     var gs = getGameBoardSize();
     ball.size = 15;
     ball.pos = { x: gs.vp.w/2 - ball.size/2, y: gs.vp.h/2 - ball.size/2 };
-    ball.dir = { x: -5, y: 4 };
-
-
-    // for debug
-    // var global1 = { x: 0, y: 0 };
-    // var global2 = { x: 0, y: 0 };
-    // var global3 = { x: 0, y: 0 };
-
-
+    ball.dir = { x: -6, y: 5 };
 
     function updateScene(delta) {
       var PLAYER_SPEED = 0.0005;
       var gs = getGameBoardSize();
-
       updateScore(); //updets score count
-
       // Update player
       players.forEach(function(player) {
         player.pos -= player.left * delta * PLAYER_SPEED;
         player.pos += player.right * delta * PLAYER_SPEED;
-
         if (player.pos > 1) player.pos = 1;
         if (player.pos < 0) player.pos = 0;
       });
 
-
       // Update ball
       ball.pos = addVector(ball.pos, ball.dir);
-
       // detect hits with players
       players.forEach(function(player) {
         var pos = player.getPos(gs);
-
         var hit = false;
         var hitDistance = player.size + ball.size;
         var incoming = subPos(pos, ball.pos);
@@ -188,52 +159,28 @@
         var yDistance= 0;
         var xdistance= 0;
 
-
        if (pos.y-125 <= ball.pos.y+ball.size && pos.y+ 125 >= ball.pos.y+ball.size) {
           if (ball.pos.x - ball.size < gs.x + 20) {
             ball.pos.x = gs.x + ball.size+30;
             ball.dir.x = -ball.dir.x;
             hit = true;
-
           }
           else if (ball.pos.x + ball.size > gs.x + gs.w-30) {
             ball.pos.x = gs.w - ball.size + gs.x-30;
             ball.dir.x = -ball.dir.x;
             hit = true;
-
           }
         }
-        /*    if (distance <= hitDistance) {
-         var normIncoming = normalizeVector(incoming);
-          var normDir = normalizeVector(ball.dir);
-          var normTangent = rotateVector(normIncoming, Math.PI/2);
-
-          var angle = angleVector(normDir, normTangent);
-          var normNewDir = rotateVector(normTangent, angle);
-
-          // for debug
-          // global1 = cpyVector(normDir);
-          // global2 = cpyVector(normTangent);
-          // global3 = cpyVector(normNewDir);
-
-          ball.dir = mulVector(normNewDir, lenVector(ball.dir));
-
-          hit = true;
-
-        } */
-
-        player.isHit = hit;
+            player.isHit = hit;
       });
 
       // detect hits with game border
       if (ball.pos.x + ball.size > gs.x + gs.w) {
-      //  ball.pos.x = gs.w - ball.size + gs.x;
-      //  ball.dir.x = -ball.dir.x;
+
        wallL=1;
        reRun1();
-      } else if (ball.pos.x - ball.size < gs.x) {
-      //  ball.pos.x = gs.x + ball.size;
-        //ball.dir.x = -ball.dir.x;
+      }
+      else if (ball.pos.x - ball.size < gs.x) {
        wallR=1;
        reRun2();
       }
@@ -254,56 +201,57 @@
          ctx.textAlign = "left";
          ctx.textBaseline = "top";
          ctx.fillText(point2 + "  :  " + point1, gs.vp.w/2-100, 20 );
-
       }
-
-
     function reRun1() {
       if (point2 < 4 && wallL==1){
    		point2 ++;
    		wallL=0;
         var gs = getGameBoardSize();
-      ball.pos = { x:  ball.size/2+20, y:  ball.size/2 +20};
-      ball.dir = { x: 5, y: 4 }; // cheek to see if the ball starts from where it ended
-
+      ball.pos = { x:  ball.size/2+45, y:  ball.size/2 +20};
+      ball.dir = { x: 6, y: 5 }; // cheek to see if the ball starts from where it ended
    	}
    	else {
    		gameOver();
    	}
-
   }
+
     function reRun2() {
       if (point1 < 4 && wallR==1){
         point1 ++;
         wallR=0;
          var gs = getGameBoardSize();
-        ball.pos = { x: gs.vp.w-ball.size/2-20, y: ball.size/2+20 };
-        ball.dir = { x: -5, y: 4 }; // cheek to see if the ball starts from where it ended
-
-      }
+        ball.pos = { x: gs.vp.w-ball.size/2-85, y: ball.size/2+20 };
+        ball.dir = { x: -6, y: 5 }; // cheek to see if the ball starts from where it ended
+    }
       else {
           gameOver();
         }
     }
 
     function gameOver() {
-    if (wallR== 1) {
-      point1++;
+     if (wallR== 1) {
+       point1++;
 
-    }
-    else if (wallL == 1) {
+     }
+     else if (wallL == 1) {
       point2++;
-
     }
-      //cancel animation
       over = true;
       GameOver = true;
-      //drawRestart();
-      //restartBtn.draw();
+    }
+
+    // Function for creating particles object
+    function createParticles(x, y, m) {
+    	this.x = x || 0;
+    	this.y = y || 0;
+
+    	this.radius = 1.2;
+
+    	this.vx = -1.5 + Math.random()*3;
+    	this.vy = m * Math.random()*1.5;
     }
 
     // reStart Button function
-
        restartBtn = {
          w: 100,
          h: 50,
@@ -339,8 +287,6 @@
              ctx.textAlign = "center";
              ctx.textBaseline = "middle";
              ctx.fillText("GAME OVER ", gs.vp.w/2, gs.vp.h/2 - 150 );
-
-
              ctx.strokeStyle = 'rgb(255, 255, 26)';
              ctx.lineWidth = "2";
              ctx.strokeRect(gs.vp.w/2-100, gs.vp.h/2-50, 200, 100);
@@ -352,19 +298,16 @@
          }
        };
 
-       startBtn = {
+     startBtn = {
          w: 100,
          h: 50,
          x: gs.vp.w/2-100,
          y: gs.vp.h/2-50,
-
          draw: function() {
-
            var gs = getGameBoardSize();
            ctx.strokeStyle = "white";
            ctx.lineWidth = "2";
            ctx.strokeRect(gs.vp.w/2-100, gs.vp.h/2-50, 200, 100);
-           var gs = getGameBoardSize();
            ctx.font = "40px Arial, sans-serif";
            ctx.textAlign = "center";
            ctx.textBaseline = "middle";
@@ -372,43 +315,34 @@
            ctx.fillText(" START", gs.vp.w/2, gs.vp.h/2  );
            ctx.fillText("Connect your mobile device to control ", gs.vp.w/2, gs.vp.h/2-200  );
            ctx.fillText(" the left and right paddels and press start START", gs.vp.w/2, gs.vp.h/2-160  );
-         }
+          }
        };
 
 
     function btnClick(e) {
-
            // Variables for storing mouse position on click
            var mx = e.pageX,
                my = e.pageY;
-
            // Click start button
            if(mx >= startBtn.x  && mx <= startBtn.x + startBtn.w) {
               over = false;
               startScreen();
            }
-
-   // If the game is over, and the restart button is clicked
+           // If the game is over, and the restart button is clicked
           if(GameOver == true) {
              if(mx >= restartBtn.x && mx <= restartBtn.x + restartBtn.w) {
-
               point1 = 0;
               point2 = -1;
               wallR = 0;
               wallL = 0;
-
               var gs = getGameBoardSize();
               ball.pos = { x: gs.vp.w - ball.size/2, y: gs.vp.h + ball.size/2 };
               ball.dir = { x: -5, y: 4 };
-
               over = false;
               GameOverover = false;
               }
            }
-  }
-
-
-
+    }
 
     function addVector(a, b) {
       return { x: a.x + b.x, y: a.y + b.y };
@@ -450,10 +384,8 @@
         x: a.x * Math.cos(angle) - a.y * Math.sin(angle),
         y: a.x * Math.sin(angle) + a.y * Math.cos(angle),
       };
-    }
+     }
 
-
-//startScreen();
 // starts the hole game
   function startScreen(){
       if (over==true) {
@@ -467,17 +399,15 @@
                //  drawstartBtn();
              startBtn.draw();
               }
-              else if (GameOver == true){
+          else if (GameOver == true){
                 restartBtn.draw();
               }
-
         }
       else {
         updateScene(delta);
         drawScene();
       }
-
-          }
+   }
 
 
     function drawScene() {
@@ -493,39 +423,32 @@
 
       drawBall(ball);
       updateScore();
-
-      // for debug
-      // drawStuff(gs);
     }
 
     function drawBorder(vp) {
 
       ctx.fillStyle = 'white';
-      ctx.fillRect(8, 8, 4, vp.h-20);
-      ctx.fillRect(8, 8, vp.w-20, 4);
-      ctx.fillRect(vp.w-16, 8, 4, vp.h-20);
-      ctx.fillRect(8, vp.h-16, vp.w-20, 4);
+      ctx.fillRect(gs.x, 8, 4, vp.h-20);
+      ctx.fillRect(gs.x, 8, vp.w-80, 4);
+      ctx.fillRect(vp.w-40, 8, 4, vp.h-20);
+     ctx.fillRect(gs.x, vp.h-16, vp.w-80, 4);
       ctx.fillRect(vp.w/2, 8, 4, vp.h-20);
     }
 
     function drawBorder2(vp) {
       ctx.clearRect(0, 0, gs.vp.w, gs.vp.h);
       ctx.fillStyle = 'white';
-      ctx.fillRect(8, 8, 4, vp.h-20);
-      ctx.fillRect(8, 8, vp.w-20, 4);
-      ctx.fillRect(vp.w-16, 8, 4, vp.h-20);
-     ctx.fillRect(8, vp.h-16, vp.w-20, 4);
-    // ctx.fillRect(vp.w/2, 8, 4, vp.h-20);
+      ctx.fillRect(gs.x, 8, 4, vp.h-20);
+      ctx.fillRect(gs.x, 8, vp.w-80, 4);
+      ctx.fillRect(vp.w-40, 8, 4, vp.h-20);
+     ctx.fillRect(gs.x, vp.h-16, vp.w-80, 4);
+
     }
 
     function drawPlayer(gs, player) {
-    //  if (player.isHit) ctx.fillStyle = 'rgba(0, 0, 0, 0)';
-    //  else
-       ctx.fillStyle = player.color;
+
+      ctx.fillStyle = player.color;
       var pos = player.getPos(gs);
-      //ctx.beginPath();
-      //ctx.arc(pos.x, pos.y, player.size, 0, Math.PI*2, false);
-      //ctx.fill();
       ctx.fillRect(pos.x,pos.y-125,player.w,player.h);
 
     }
@@ -563,23 +486,15 @@
          var now = new Date();
          delta = now - prevTime;
          prevTime = now;
-
-         //updateScene(delta);
-         //drawScene();
          startScreen();
        }, 15);
-
-
   }
 
   function setupPlayer() {
     var canvas = document.getElementById('game-player');
     var playerNameTag = document.getElementById('player-name');
-
     var playerName;
-
-    socket.emit('join game', { gameName: 'adam' });
-
+    socket.emit('join game', { gameName: 'lily' });
     socket.on('game joined', function(data) {
       console.log('game joined', data);
       canvas.style.backgroundColor = data.playerColor;
@@ -591,8 +506,7 @@
       var alpha = event.alpha; // direction
       var beta = event.beta; // tilt front-back
       var gamma = event.gamma; // tilt left-right
-
-      var data = { gameName: 'adam', playerName: playerName, alpha: alpha, beta: beta, gamma: gamma };
+      var data = { gameName: 'lily', playerName: playerName, alpha: alpha, beta: beta, gamma: gamma };
       socket.emit('orientation data', data);
     });
   }
